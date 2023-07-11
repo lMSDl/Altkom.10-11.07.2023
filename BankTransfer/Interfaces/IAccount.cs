cca8c2e126a8de1c683eb23d60817417d26130a7
Paweł Łęczycki <pawel@msd.info.pl>
2023-07-11 15:38:53 +0200

    BankTransfer

---
+++ BankTransfer/Interfaces/IAccount.cs
@@ -0,0 +1,20 @@
+﻿using BankTransfer.Models;
+
+namespace BankTransfer.Interfaces
+{
+    public interface IAccount
+    {
+        string AccountNumber { get; }
+        double Balance { get; }
+
+
+
+        void AddTransaction(Transaction transaction);
+
+        IEnumerable<Transaction> GetTransactions();
+        IEnumerable<Transaction> FilterTransactions(TransactionType? type, IAccount account);
+
+        Task TransferAsync(IAccount toAccount, double amount, ITransactionProvider provider);
+
+    }
+}
